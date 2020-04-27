c890f00691374daae88834011e4f2264afd81de7
Zhi Qin Tan <zhiqin1998@hotmail.com>
2020-04-27 12:59:53 +0800

    proper main function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from src.fraction import Fraction
 
 
+def evaluate(c, x, r):
+    n = len(c) - 1
+    temp = c[n]
+    for i in range(n - 1, -1, -1):
+        temp = temp * (r - x[i]) + c[i]
+    return temp
+
+
 def newton_coef(x, y):
     n = len(x)
     a = []
@@ -14,11 +22,39 @@ def newton_coef(x, y):
     return a
 
 
+def parse(number):
+    if number.count('/') == 1:
+        number = number.split('/')
+        return Fraction(int(number[0]), int(number[1]))
+    elif '.' in number:
+        number = float(number).as_integer_ratio()
+        return Fraction(number[0], number[1])
+    else:
+        return Fraction(int(number))
+
+
+def get_polynomial_str(c, x):
+    # TODO create function to get string representation of polynomial
+    pass
+
+
 def main():
-    x = [Fraction(-5), Fraction(-1), Fraction(0), Fraction(2)]
-    y = [Fraction(-2), Fraction(6), Fraction(1), Fraction(3)]
+    xs = input('enter x values (separated by spaces): ').split()
+    ys = input('enter y values (separated by spaces): ').split()
+    assert len(xs) == len(ys)
+    x = list(map(parse, xs))
+    y = list(map(parse, ys))
     coef = newton_coef(x, y)
-    print(coef)
+    print('coefficient of the newton polynomial is', ' '.join([str(c) for c in coef]))
+    print('resulting polynomial is:')
+    print(get_polynomial_str(coef, x))
+    while True:
+        r = input('enter value to interpolate at (q to exit): ')
+        if r == 'q':
+            break
+        r = parse(r)
+        ans = evaluate(coef, x, r)
+        print('value at {} is {}'.format(r, ans))
 
 
 if __name__ == '__main__':
